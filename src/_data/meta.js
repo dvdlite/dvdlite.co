37829392d11da9903908d38a4ee2e9ef036bd2ff
David Leitko <david@leitko.net>
2024-02-19 21:37:37 -0500

    Initial personalization

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,17 +1,17 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'dvdlite.co',
   siteDescription:
-    'Eleventy starter built around the CSS workflow suggested by buildexcellentwebsit.es',
+    'Technology and MakerSpace',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
   author: {
-    name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+    name: 'David Leitko', // i.e. Lene Saile - page / blog author's name. Must be set.
     avatar: '/favicon.png',
-    email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-    website: 'https://www.lenesaile.com' // i.e. https.://www.lenesaile.com - the personal site of the author
+    email: 'dvdlite@dvdlite.co', // i.e. hola@lenesaile.com - email of the author
+    website: 'https://dvdlite.co' // i.e. https.://www.lenesaile.com - the personal site of the author
   },
   creator: {
     name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
@@ -23,12 +23,12 @@ module.exports = {
   themeBgColor: '#FBFBFB', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   opengraph_default: '/assets/images/template/opengraph-default.jpg', // fallback/default meta image
   opengraph_default_alt:
-    'Visible content: Eleventy starter built around the CSS workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es', // alt text for default meta image
+    'Visible content: Technology and MakerSpace', // alt text for default meta image
   blog: {
     // RSS feed
-    name: 'My Web Development Blog',
+    name: 'dvdlite.co blog',
     description:
-      'Tell the word what you are writing about in your blog. It will show up on feed readers.',
+      'Technology and MakerSpace',
     // feed links are looped over in the head. You may add more to the array.
     feedLinks: [
       {
