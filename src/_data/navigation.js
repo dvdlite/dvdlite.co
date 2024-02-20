9d3ade5b7dac19f3f6531c33ecb0424da8f906cd
David Leitko <david@leitko.net>
2024-02-19 22:28:26 -0500

    Navigation cleanup

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -5,21 +5,9 @@ module.exports = {
       url: '/about/'
     },
     {
-      text: 'Get started',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
       text: 'Blog',
       url: '/blog/'
     },
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    }
   ],
   bottom: [
     {
